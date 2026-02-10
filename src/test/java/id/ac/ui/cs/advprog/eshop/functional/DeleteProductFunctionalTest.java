8c886752e7b2024a93d48bda4cffd873296fa12d
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-02-10 10:16:17 +0700

    test: remove code redundancy on functional test

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/DeleteProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/DeleteProductFunctionalTest.java
@@ -1,39 +1,17 @@
 package id.ac.ui.cs.advprog.eshop.functional;
 
-import io.github.bonigarcia.seljup.SeleniumJupiter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@ExtendWith(SeleniumJupiter.class)
-public class DeleteProductFunctionalTest {
-    @LocalServerPort
-    private int serverPort;
-
-    @Value("${app.baseUrl:http://localhost}")
-    private String testBaseUrl;
-
-    private String baseUrl;
-
-    @BeforeEach
-    void setUpTest() {
-        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
-    }
-
+public class DeleteProductFunctionalTest extends BaseFunctionalTest {
     @Test
     void deleteProductFlow_isCorrect(ChromeDriver driver) throws Exception {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
